fd09f7d648f76941e5c8e0d65b40b5a223df1e49
liurong <570899307@qq.com>
2014-12-30 10:46:44 -0500

    change player-tests_defined_global

--- __tests__/player-tests.js
+++ __tests__/player-tests.js
@@ -5,19 +5,22 @@ jest.dontMock('../src/model/defense');
 
 
 describe('Player', function() {
+  var Role,role,Weapon,weapon,Defense,defense,Player,player;
+  beforeEach(function () {
+    Role = require('../src/model/role');
+    role = new Role('狂战士', 5);
+
+    Weapon = require('../src/model/weapon');
+    weapon = new Weapon('用砍刀', 5);
+
+    Defense = require('../src/model/defense');
+    defense = new Defense('藤甲', 5);
+
+    Player = require('../src/model/player');
+    player = new Player('李四', 50, role, weapon, defense);
+  });
+  
   describe('#getRoleName', function() {
-    var Role = require('../src/model/role');
-    var role = new Role('狂战士', 5);
-
-    var Weapon = require('../src/model/weapon');
-    var weapon = new Weapon('用砍刀', 5);
-
-    var Defense = require('../src/model/defense');
-    var defense = new Defense('藤甲', 5);
-
-    var Player = require('../src/model/player');
-    var player = new Player('李四', 50, role, weapon, defense);
-
     it('should return roleName', function() {
       var result = player.getRoleName();
       expect(result).toEqual('狂战士');

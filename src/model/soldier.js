57b2bd9f348981ed8009637fdcd5c26ee90b3f5a
liurong <570899307@qq.com>
2014-12-30 15:16:16 -0500

    add soldier.js

---
+++ src/model/soldier.js
@@ -0,0 +1,31 @@
+function Soldier (name, hp, role, weapon, defense) {
+  this.name = name;
+  this.hp = hp;
+  this.role = role;
+  this.weapon = weapon;
+  this.defense = defense;
+}
+
+Player.prototype.getRoleName = function () {
+  return this.role.roleName;
+};
+
+Player.prototype.getRoleAttack = function () {
+  return this.role.roleAttack;
+};
+
+Player.prototype.getWeaponName = function () {
+  return this.weapon.weaponName;
+};
+
+Player.prototype.getWeaponAttack = function () {
+  return this.weapon.weaponAttack;
+};
+
+Player.prototype.getdefenseName = function () {
+  return this.defense.defenseName;
+};
+
+Player.prototype.getDefenseAttack = function () {
+  return this.defense.defenseAttack;
+};
